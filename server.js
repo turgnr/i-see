17ebd4d1b6a663be537aab0dff1843a9ead98d97
dolev1894 <dolevda@ac.sce.ac.il>
2020-05-05 11:18:37 +0300

    fix db

--- server.js
+++ server.js
@@ -5,36 +5,27 @@ const MongoClient = require('mongodb').MongoClient;
 const URL = 'mongodb+srv://dolev:205577034Dd@isee-4oc3b.mongodb.net/test?retryWrites=true&w=majority';
 
 app.use(bodyParser.urlencoded({ extended: true }));
+const items = {
+    "name": "orv",
+    "x": "3",
+    "y": "4",
+    "description": "ddd"
+};
 
-MongoClient.connect(URL,{ useUnifiedTopology:
-true})
-    .then (client => {
+MongoClient.connect(URL, {
+    useUnifiedTopology:
+        true
+})
+    .then(client => {
         console.log('Connected to Database')
         const db = client.db('LOCATION')
         const locationCollection = db.collection('locations')
         app.use(bodyParser.urlencoded({ extended: true }));
-        app.get('/', (req, res) => {
-            db.collection('locations').find().toArray()
-                .then(results => {
-                    console.log(results)
-                    res.sendFile(__dirname + '/index.html')
-
-                })
-                .catch(error => console.error(error))
-        })
-
-        app.post('/location', (req, res) => {
-            locationCollection.insertOne(req.body)
-                .then(result => {
-                    res.redirect('/')
-                })
-                .catch(error => console.error(error))
-        })
-        app.listen(3000, function () {
-            console.log('listening on 3000')
-    })
+        locationCollection.insertOne(items)
+            .then(result => console.log(`Successfully inserted item with _id: ${result.insertedId}`))
+            .catch(err => console.error(`Failed to insert item: ${err}`))
 
 
-}).catch(error => console.error(error))
+    }).catch(error => console.error(error))
 
 
