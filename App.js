e9a62615633bfd0f56851a4013c060a2a327a5a4
eliran gabay <34740857+EliranGabay@users.noreply.github.com>
2020-05-21 22:12:18 +0300

    disableYellowBox

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { PropTypes, Component, useEffect } from "react";
 import Navigator from "./homeStack";
 
+console.disableYellowBox = true;
+
 export default function App() {
   return <Navigator />;
 }
